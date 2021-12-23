c81854ce4ea323b6282ae366df392ccdf020bb92
Baozier <byn9826@gmail.com>
2021-12-22 21:57:40 -0500

    Commands to backup and restore database (#45)

--- server/services/syncPerformance.ts
+++ server/services/syncPerformance.ts
@@ -23,42 +23,49 @@ export const calcAveragePrice = async (): Promise<tickerModel.Ticker[]> => {
     for (const tickerDaily of tickerDailyRecords) {
       index++
 
+      let hasUpdate = false
       let weeklyAverage = tickerDaily.weeklyAveragePrice
       if (index >= 5 && tickerDaily.weeklyAveragePrice === null) {
         const relatedDaily = tickerDailyRecords.slice(index - 5, index)
         weeklyAverage = calcAverage(relatedDaily)
+        hasUpdate = true
       }
 
       let monthlyAverage = tickerDaily.monthlyAveragePrice
       if (index >= 20 && tickerDaily.monthlyAveragePrice === null) {
         const relatedDaily = tickerDailyRecords.slice(index - 20, index)
         monthlyAverage = calcAverage(relatedDaily)
+        hasUpdate = true
       }
 
       let quarterlyAverage = tickerDaily.quarterlyAveragePrice
       if (index >= 80 && tickerDaily.quarterlyAveragePrice === null) {
         const relatedDaily = tickerDailyRecords.slice(index - 80, index)
         quarterlyAverage = calcAverage(relatedDaily)
+        hasUpdate = true
       }
 
       let yearlyAverage = tickerDaily.yearlyAveragePrice
       if (index >= 320 && tickerDaily.yearlyAveragePrice === null) {
         const relatedDaily = tickerDailyRecords.slice(index - 320, index)
         yearlyAverage = calcAverage(relatedDaily)
+        hasUpdate = true
       }
 
-      await tickerDailyModel.update(tickerDaily.id, {
-        weeklyAveragePrice: weeklyAverage,
-        monthlyAveragePrice: monthlyAverage,
-        quarterlyAveragePrice: quarterlyAverage,
-        yearlyAveragePrice: yearlyAverage
-      })
+      if (hasUpdate) {
+        await tickerDailyModel.update(tickerDaily.id, {
+          weeklyAveragePrice: weeklyAverage,
+          monthlyAveragePrice: monthlyAverage,
+          quarterlyAveragePrice: quarterlyAverage,
+          yearlyAveragePrice: yearlyAverage
+        })
+      }
     }
   }
   return tickers
 }
 
-export const calculateDaily = async (): Promise<tickerModel.Ticker[]> => {
+export const calcPriceMovement = async (): Promise<tickerModel.Ticker[]> => {
   const tickers = await tickerModel.getAll()
 
   for (const ticker of tickers) {
