7d7cf7a69072f26e5305c587bbfa96dd931071fe
baozier <byn9826@gmail.com>
2022-03-07 16:28:15 -0500

    remove console

--- client/src/tools/locale.ts
+++ client/src/tools/locale.ts
@@ -1,7 +1,6 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import enResource from '../locales/en.json'
-console.log(enResource)
 
 export const init = () => i18n
   .use(initReactI18next)
