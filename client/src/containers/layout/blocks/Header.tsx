306c6cbaca2312596c85023cd9450f06bfa6f4d6
byn9826 <byn9826@gmail.com>
2022-03-07 21:05:19 -0500

    Basic react-jss setup

--- client/src/containers/layout/blocks/Header.tsx
+++ client/src/containers/layout/blocks/Header.tsx
@@ -1,18 +1,37 @@
 import { Link } from 'react-router-dom'
+import { createUseStyles } from 'react-jss'
+import classNames from 'classnames'
 import { Icon, Label } from 'semantic-ui-react'
 import * as routerConstant from '../../../constants/router'
+import * as themeConstant from '../../../constants/theme'
 import * as localeTool from '../../../tools/locale'
 
+const useStyles = createUseStyles((theme: themeConstant.THEME) => ({
+  header: {
+    height: 50,
+    position: 'fixed',
+    top: 0,
+    width: '100%',
+    backgroundColor: theme.PRIMARY_COLOR,
+    paddingLeft: theme.SPACING * 2,
+    paddingRight: theme.SPACING * 2,
+  },
+}))
+
 const Header = () => {
+  const classes = useStyles()
+
   return (
-    <nav>
-      <Link to={routerConstant.NAV.TOP_TRADERS}>
-        <Label>
-          <Icon name="chart line" />
-          {localeTool.t('topTraders.title')}
-        </Label>
-      </Link>
-    </nav>
+    <header className={classNames('row-between', classes.header)}>
+      <nav>
+        <Link to={routerConstant.NAV.TOP_TRADERS}>
+          <Label>
+            <Icon name="chart line" />
+            {localeTool.t('topTraders.title')}
+          </Label>
+        </Link>
+      </nav>
+    </header>
   )
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'react-jss'
 import Router from './containers/Router'
 import * as localTool from './tools/locale'
+import * as themeConstants from './constants/theme'
 
 import './index.css'
 import 'semantic-ui-css/semantic.min.css'
@@ -10,7 +12,9 @@ localTool.init()
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router />
+    <ThemeProvider theme={themeConstants.BASIC}>
+      <Router />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
