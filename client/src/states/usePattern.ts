7223f42c2bb6b9268aaca1c0251e04d00e16c174
baozier <byn9826@gmail.com>
2022-03-09 17:27:47 -0500

    Build api to get top patterns

--- client/src/states/usePattern.ts
+++ client/src/states/usePattern.ts
@@ -1,4 +1,4 @@
-import * as interfaces from '@interfaces'
+import * as interfaces from '@shared/interfaces'
 import { useContext, useState } from 'react'
 import { context, Context } from './context'
 import * as requestAdpater from '../adapters/request'
@@ -6,7 +6,7 @@ import * as routerConstant from '../constants/router'
 
 const usePattern = () => {
   const store: Context = useContext(context)
-  const [topPatterns, setTopPatterns] = useState<interfaces.patternsResponse.Top>(null)
+  const [topPatterns, setTopPatterns] = useState<interfaces.patternsResponse.Top | null>(null)
 
   const fetchTopPatterns = async () => {
     const endpoint = `${routerConstant.API.PATTERNS}/top`
