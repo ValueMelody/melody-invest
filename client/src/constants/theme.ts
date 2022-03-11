97ace9d299c998644dcfd274f643de6b1cea9b42
Baozier <byn9826@gmail.com>
2022-03-10 21:08:56 -0500

    Show buy and sell behavior for top patterns (#93)

--- client/src/constants/theme.ts
+++ client/src/constants/theme.ts
@@ -1,9 +1,11 @@
 export interface Theme {
   PRIMARY_COLOR: string;
+  SECONDARY_COLOR: string;
   SPACING: number;
 }
 
 export const BASIC: Theme = {
-  PRIMARY_COLOR: '#428af5',
+  PRIMARY_COLOR: '#2185d0',
+  SECONDARY_COLOR: '#767676',
   SPACING: 16,
 }
