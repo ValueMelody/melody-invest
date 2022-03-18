218cd0b17ef1e97898ab1c140bda345fd421a557
Baozier <byn9826@gmail.com>
2022-03-17 22:24:00 -0400

    Ticker table migration and seed (#113)

---
+++ server/migrations/20220317023850_create_ticker_table.js
@@ -0,0 +1,23 @@
+exports.up = (knex) => {
+  return knex.schema
+    .createTable('ticker', (table) => {
+      table.increments('id')
+      table.string('name', 50).notNullable()
+      table.string('symbol', 10).notNullable()
+      table.specificType('region', 'CHAR(2)').notNullable()
+      table.specificType('firstPriceDate', 'CHAR(10)')
+      table.specificType('lastPriceDate', 'CHAR(10)')
+      table.specificType('firstEPSYear', 'CHAR(4)')
+      table.specificType('lastEPSYear', 'CHAR(4)')
+      table.specificType('firstIncomeYear', 'CHAR(4)')
+      table.specificType('lastIncomeYear', 'CHAR(4)')
+      table.specificType('firstIncomeQuarter', 'CHAR(7)')
+      table.specificType('lastIncomeQuarter', 'CHAR(7)')
+      table.specificType('quarterlyEPSMonthDiffer', 'INT2')
+      table.unique(['symbol', 'region'], 'ticker_ukey')
+    })
+}
+
+exports.down = (knex) => {
+  return knex.schema.dropTable('ticker')
+}
