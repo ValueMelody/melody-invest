4beca029caf1b957ff40832dc88b235ad6ea1211
Baozier <byn9826@gmail.com>
2022-04-11 15:43:02 -0400

    Add tests for backend verify and run tools

---
+++ server/tools/verify.test.ts
@@ -0,0 +1,10 @@
+import * as verify from './verify'
+
+test('could test email', async () => {
+  expect(verify.isEmail('a@email.com')).toBeTruthy()
+  expect(verify.isEmail('abc@outlook.com')).toBeTruthy()
+  expect(verify.isEmail('abc@outlook')).toBeFalsy()
+  expect(verify.isEmail('@outlook.com')).toBeFalsy()
+  expect(verify.isEmail('abcoutlook.com')).toBeFalsy()
+  expect(verify.isEmail('abc@outlook.')).toBeFalsy()
+})
