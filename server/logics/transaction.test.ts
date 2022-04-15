4d3c1de3f3a15eba18bcb4f0fbe745a31dc568b3
Baozier <byn9826@gmail.com>
2022-04-15 13:54:13 -0400

    refactor rebalcne logic

---
+++ server/logics/transaction.test.ts
@@ -0,0 +1,383 @@
+import * as interfaces from '@shared/interfaces'
+import * as transaction from './transaction'
+
+const DAILY_TICKERS: interfaces.dailyTickersModel.DailyTickers = {
+  1: {
+    // @ts-ignore
+    daily: { closePrice: 50, splitMultiplier: 1000 },
+  },
+  2: {
+    // @ts-ignore
+    daily: { closePrice: 100, splitMultiplier: 5 },
+  },
+}
+
+const HOLDING_HOLDINGS: interfaces.traderHoldingModel.Holding[] = [
+  { tickerId: 1, shares: 100, splitMultiplier: 100, value: 10000 },
+  { tickerId: 2, shares: 200, splitMultiplier: 200, value: 40000 },
+  { tickerId: 3, shares: 300, splitMultiplier: 300, value: 90000 },
+]
+
+const HOLDING_DETAILS: transaction.HoldingDetails = {
+  totalCash: 10000,
+  totalValue: 140000,
+  holdings: [HOLDING_HOLDINGS[0], HOLDING_HOLDINGS[1], HOLDING_HOLDINGS[2]],
+}
+
+test('could refresh holding based on daily value', () => {
+  expect(transaction.refreshHoldingToDailyValue(
+    HOLDING_HOLDINGS[0], DAILY_TICKERS[1],
+  )).toEqual({ tickerId: 1, shares: 100, splitMultiplier: 1000, value: 5000000 })
+
+  expect(transaction.refreshHoldingToDailyValue(
+    HOLDING_HOLDINGS[0], null,
+  )).toEqual(HOLDING_HOLDINGS[0])
+
+  expect(transaction.refreshHoldingToDailyValue(
+    HOLDING_HOLDINGS[1], DAILY_TICKERS[1],
+  )).toEqual({ tickerId: 2, shares: 200, splitMultiplier: 1000, value: 10000000 })
+
+  expect(transaction.refreshHoldingToDailyValue(
+    HOLDING_HOLDINGS[1], null,
+  )).toEqual(HOLDING_HOLDINGS[1])
+})
+
+test('could update holding details based on daily value', () => {
+  expect(transaction.detailsFromCashAndHoldings(
+    15,
+    [HOLDING_HOLDINGS[0], HOLDING_HOLDINGS[1], HOLDING_HOLDINGS[2]],
+    DAILY_TICKERS,
+  )).toEqual({
+    totalCash: 15,
+    totalValue: 5190015,
+    holdings: [
+      { tickerId: 1, shares: 100, splitMultiplier: 1000, value: 5000000 },
+      { tickerId: 2, shares: 200, splitMultiplier: 5, value: 100000 },
+      { tickerId: 3, shares: 300, splitMultiplier: 300, value: 90000 },
+    ],
+  })
+})
+
+test('could generate empty holding detail', () => {
+  expect(transaction.detailsFromCashAndHoldings(
+    1000,
+    [],
+    DAILY_TICKERS,
+  )).toEqual({
+    totalCash: 1000,
+    totalValue: 1000,
+    holdings: [],
+  })
+})
+
+test('should not refresh holding if holding equal to or more than ticker min percent', () => {
+  expect(transaction.refreshHoldingsForLessThanMinPercent(
+    HOLDING_DETAILS,
+    HOLDING_HOLDINGS[0],
+    5,
+    5,
+    20000,
+  )).toEqual(null)
+  expect(transaction.refreshHoldingsForLessThanMinPercent(
+    HOLDING_DETAILS,
+    HOLDING_HOLDINGS[0],
+    5.1,
+    5,
+    20000,
+  )).toEqual(null)
+})
+
+test('should not sell holding less than ticker min percent but exceed cash max precent', () => {
+  expect(transaction.refreshHoldingsForLessThanMinPercent(
+    HOLDING_DETAILS,
+    HOLDING_HOLDINGS[0],
+    5,
+    5,
+    19999,
+  )).toEqual(null)
+  expect(transaction.refreshHoldingsForLessThanMinPercent(
+    HOLDING_DETAILS,
+    HOLDING_HOLDINGS[0],
+    5.1,
+    5,
+    20000,
+  )).toEqual(null)
+})
+
+test('could refresh holdings for holding less than ticker min percent', () => {
+  expect(transaction.refreshHoldingsForLessThanMinPercent(
+    HOLDING_DETAILS,
+    HOLDING_HOLDINGS[0],
+    5,
+    6,
+    20000,
+  )).toEqual({
+    totalCash: 20000,
+    totalValue: 140000,
+    holdings: [
+      { tickerId: 2, shares: 200, splitMultiplier: 200, value: 40000 },
+      { tickerId: 3, shares: 300, splitMultiplier: 300, value: 90000 },
+    ],
+  })
+})
+
+test('should not sell holding if equal to or less than ticker max percent', () => {
+  expect(transaction.refreshHoldingsForMoreThanMaxPercernt(
+    HOLDING_DETAILS,
+    HOLDING_HOLDINGS[0],
+    16,
+    16,
+    11401,
+    DAILY_TICKERS[1].daily,
+  )).toEqual(null)
+
+  expect(transaction.refreshHoldingsForMoreThanMaxPercernt(
+    HOLDING_DETAILS,
+    HOLDING_HOLDINGS[0],
+    16,
+    17,
+    11401,
+    DAILY_TICKERS[1].daily,
+  )).toEqual(null)
+})
+
+test('should not sell holding if exceed cash max percent', () => {
+  expect(transaction.refreshHoldingsForMoreThanMaxPercernt(
+    HOLDING_DETAILS,
+    HOLDING_HOLDINGS[0],
+    16,
+    16,
+    11400,
+    DAILY_TICKERS[1].daily,
+  )).toEqual(null)
+
+  expect(transaction.refreshHoldingsForMoreThanMaxPercernt(
+    HOLDING_DETAILS,
+    HOLDING_HOLDINGS[0],
+    16,
+    16,
+    11399,
+    DAILY_TICKERS[1].daily,
+  )).toEqual(null)
+})
+
+test('could refresh holdings for holding greater than ticker max percent', () => {
+  expect(transaction.refreshHoldingsForMoreThanMaxPercernt(
+    HOLDING_DETAILS,
+    HOLDING_HOLDINGS[0],
+    16,
+    15,
+    11401,
+    DAILY_TICKERS[1].daily,
+  )).toEqual({
+    totalCash: 11400,
+    totalValue: 140000,
+    holdings: [
+      { tickerId: 1, shares: 99.972, splitMultiplier: 1000, value: 4998600 },
+      { tickerId: 2, shares: 200, splitMultiplier: 200, value: 40000 },
+      { tickerId: 3, shares: 300, splitMultiplier: 300, value: 90000 },
+    ],
+  })
+})
+
+test('should have no transaction if should not rebalance', () => {
+  expect(transaction.detailsAfterRebalance(
+    false,
+    HOLDING_DETAILS,
+    DAILY_TICKERS,
+    8,
+    65,
+    140000,
+  )).toEqual({
+    hasTransaction: false,
+    holdingDetails: HOLDING_DETAILS,
+  })
+})
+
+test('should have no transaction if not triggerring max/min percernt rule', () => {
+  expect(transaction.detailsAfterRebalance(
+    true,
+    HOLDING_DETAILS,
+    DAILY_TICKERS,
+    7,
+    65,
+    140000,
+  )).toEqual({
+    hasTransaction: false,
+    holdingDetails: HOLDING_DETAILS,
+  })
+})
+
+test('should have no transaction if not exceed max cash after sell', () => {
+  expect(transaction.detailsAfterRebalance(
+    true,
+    HOLDING_DETAILS,
+    DAILY_TICKERS,
+    7,
+    65,
+    139999,
+  )).toEqual({
+    hasTransaction: false,
+    holdingDetails: HOLDING_DETAILS,
+  })
+})
+
+test('could update holding details after rebalance for min percernt rule', () => {
+  expect(transaction.detailsAfterRebalance(
+    true,
+    HOLDING_DETAILS,
+    DAILY_TICKERS,
+    8,
+    65,
+    140000,
+  )).toEqual({
+    hasTransaction: true,
+    holdingDetails: {
+      totalCash: 20000,
+      totalValue: 140000,
+      holdings: [
+        { tickerId: 2, shares: 200, splitMultiplier: 200, value: 40000 },
+        { tickerId: 3, shares: 300, splitMultiplier: 300, value: 90000 },
+      ],
+    },
+  })
+})
+
+test('should not have transaction if daily ticker not persist', () => {
+  expect(transaction.detailsAfterRebalance(
+    true,
+    HOLDING_DETAILS,
+    {},
+    8,
+    64,
+    140000,
+  )).toEqual({
+    hasTransaction: false,
+    holdingDetails: HOLDING_DETAILS,
+  })
+
+  expect(transaction.detailsAfterRebalance(
+    true,
+    HOLDING_DETAILS,
+    DAILY_TICKERS,
+    7,
+    64,
+    100000000,
+  )).toEqual({
+    hasTransaction: false,
+    holdingDetails: HOLDING_DETAILS,
+  })
+})
+
+test('should have no transaction if exceel max cash rule', () => {
+  expect(transaction.detailsAfterRebalance(
+    true,
+    HOLDING_DETAILS,
+    {
+      // @ts-ignore
+      3: { daily: { closePrice: 50, splitMultiplier: 1 } },
+    },
+    7,
+    64,
+    10450,
+  )).toEqual({
+    hasTransaction: false,
+    holdingDetails: HOLDING_DETAILS,
+  })
+})
+
+test('could update holding details after rebalance for max percernt rule', () => {
+  expect(transaction.detailsAfterRebalance(
+    true,
+    HOLDING_DETAILS,
+    {
+      // @ts-ignore
+      3: { daily: { closePrice: 50, splitMultiplier: 1 } },
+    },
+    7,
+    64,
+    10451,
+  )).toEqual({
+    hasTransaction: true,
+    holdingDetails: {
+      totalCash: 10450,
+      totalValue: 140000,
+      holdings: [
+        { tickerId: 1, shares: 100, splitMultiplier: 100, value: 10000 },
+        { tickerId: 2, shares: 200, splitMultiplier: 200, value: 40000 },
+        { tickerId: 3, shares: 291, splitMultiplier: 1, value: 14550 },
+      ],
+    },
+  })
+})
+
+test('should only trigger min rule if exceel max cash after sell min holding', () => {
+  expect(transaction.detailsAfterRebalance(
+    true,
+    HOLDING_DETAILS,
+    {
+      ...DAILY_TICKERS,
+      // @ts-ignore
+      3: { daily: { closePrice: 50, splitMultiplier: 1 } },
+    },
+    8,
+    65,
+    139999,
+  )).toEqual({
+    hasTransaction: true,
+    holdingDetails: {
+      totalCash: 20000,
+      totalValue: 140000,
+      holdings: [
+        { tickerId: 2, shares: 200, splitMultiplier: 200, value: 40000 },
+        { tickerId: 3, shares: 300, splitMultiplier: 300, value: 90000 },
+      ],
+    },
+  })
+})
+
+test('could update holding details after rebalance for both min and max percernt rule', () => {
+  expect(transaction.detailsAfterRebalance(
+    true,
+    HOLDING_DETAILS,
+    {
+      ...DAILY_TICKERS,
+      // @ts-ignore
+      3: { daily: { closePrice: 50, splitMultiplier: 1 } },
+    },
+    8,
+    64,
+    20451,
+  )).toEqual({
+    hasTransaction: true,
+    holdingDetails: {
+      totalCash: 20450,
+      totalValue: 140000,
+      holdings: [
+        { tickerId: 2, shares: 200, splitMultiplier: 200, value: 40000 },
+        { tickerId: 3, shares: 291, splitMultiplier: 1, value: 14550 },
+      ],
+    },
+  })
+})
+
+// const DAILY_TICKERS: interfaces.dailyTickersModel.DailyTickers = {
+//   1: {
+//     // @ts-ignore
+//     daily: { closePrice: 50, splitMultiplier: 1000 },
+//   },
+//   2: {
+//     // @ts-ignore
+//     daily: { closePrice: 100, splitMultiplier: 5 },
+//   },
+// }
+// const HOLDING_HOLDINGS: interfaces.traderHoldingModel.Holding[] = [
+//   { tickerId: 1, shares: 100, splitMultiplier: 100, value: 10000 },
+//   { tickerId: 2, shares: 200, splitMultiplier: 200, value: 40000 },
+//   { tickerId: 3, shares: 300, splitMultiplier: 300, value: 90000 },
+// ]
+// const HOLDING_DETAILS: transaction.HoldingDetails = {
+//   totalCash: 10000,
+//   totalValue: 140000,
+//   holdings: [HOLDING_HOLDINGS[0], HOLDING_HOLDINGS[1], HOLDING_HOLDINGS[2]],
+// }
---
+++ server/logics/transaction.ts
@@ -0,0 +1,153 @@
+import * as interfaces from '@shared/interfaces'
+
+export const refreshHoldingToDailyValue = (
+  holding: interfaces.traderHoldingModel.Holding,
+  dailyTicker: interfaces.dailyTickersModel.DailyTicker | null,
+): interfaces.traderHoldingModel.Holding => {
+  const matchedDaily = dailyTicker?.daily
+  const holdingValue = matchedDaily
+    ? holding.shares * matchedDaily.closePrice * matchedDaily.splitMultiplier
+    : holding.value
+  const splitMultiplier = matchedDaily?.splitMultiplier || holding.splitMultiplier
+  const updatedHolding = {
+    tickerId: holding.tickerId,
+    shares: holding.shares,
+    value: holdingValue,
+    splitMultiplier,
+  }
+  return updatedHolding
+}
+
+export interface HoldingDetails {
+  totalCash: number,
+  totalValue: number,
+  holdings: interfaces.traderHoldingModel.Holding[]
+}
+
+export const detailsFromCashAndHoldings = (
+  totalCash: number,
+  holdings: interfaces.traderHoldingModel.Holding[],
+  dailyTickers: interfaces.dailyTickersModel.DailyTickers,
+): HoldingDetails => {
+  const detailsBeforeUpdate: HoldingDetails = {
+    totalValue: totalCash, totalCash, holdings: [],
+  }
+  return holdings.reduce((details, holding) => {
+    const matched = dailyTickers[holding.tickerId] || null
+    const refreshedHolding = refreshHoldingToDailyValue(holding, matched)
+    return {
+      totalValue: details.totalValue + refreshedHolding.value,
+      totalCash: details.totalCash,
+      holdings: [...details.holdings, refreshedHolding],
+    }
+  }, detailsBeforeUpdate)
+}
+
+export const refreshHoldingsForLessThanMinPercent = (
+  holdingDetails: HoldingDetails,
+  holdingForSell: interfaces.traderHoldingModel.Holding,
+  holdingPercent: number,
+  tickerMinPercent: number,
+  maxCashValue: number,
+): HoldingDetails | null => {
+  const isLessThanMinPercent = holdingPercent < tickerMinPercent
+  if (!isLessThanMinPercent) return null
+
+  const cashAfterSell = holdingDetails.totalCash + holdingForSell.value
+  const couldSell = cashAfterSell <= maxCashValue
+  if (!couldSell) return null
+  return {
+    totalValue: holdingDetails.totalValue,
+    totalCash: cashAfterSell,
+    holdings: holdingDetails.holdings.filter((holding) => holding.tickerId !== holdingForSell.tickerId),
+  }
+}
+
+export const refreshHoldingsForMoreThanMaxPercernt = (
+  holdingDetails: HoldingDetails,
+  holdingForSell: interfaces.traderHoldingModel.Holding,
+  holdingPercent: number,
+  tickerMaxPercent: number,
+  maxCashValue: number,
+  tickerDaily: interfaces.tickerDailyModel.Record,
+): HoldingDetails | null => {
+  const isMoreThanMaxPercent = holdingPercent > tickerMaxPercent
+  if (!isMoreThanMaxPercent) return null
+
+  const sellTargetPercent = holdingPercent - tickerMaxPercent
+  const sellTargetValue = Math.ceil(holdingDetails.totalValue * sellTargetPercent / 100)
+  const sellTargetShares = Math.ceil(sellTargetValue / tickerDaily.closePrice)
+  const sharesSold = sellTargetShares / tickerDaily.splitMultiplier
+
+  const sharesLeft = holdingForSell.shares - sharesSold
+  const dailyFinalPrice = tickerDaily.closePrice * tickerDaily.splitMultiplier
+  const cashAfterSell = holdingDetails.totalCash + sharesSold * dailyFinalPrice
+  const couldSell = sharesLeft && cashAfterSell < maxCashValue
+
+  if (!couldSell) return null
+
+  const holdingAfterSell = {
+    tickerId: holdingForSell.tickerId,
+    shares: sharesLeft,
+    value: sharesLeft * dailyFinalPrice,
+    splitMultiplier: tickerDaily.splitMultiplier,
+  }
+  return {
+    totalValue: holdingDetails.totalValue,
+    totalCash: cashAfterSell,
+    holdings: holdingDetails.holdings.map((holding) => {
+      if (holding.tickerId !== holdingForSell.tickerId) return holding
+      return holdingAfterSell
+    }),
+  }
+}
+
+export const detailsAfterRebalance = (
+  shouldRebalance: boolean,
+  currentDetails: HoldingDetails,
+  dailyTickers: interfaces.dailyTickersModel.DailyTickers,
+  tickerMinPercent: number,
+  tickerMaxPercent: number,
+  maxCashValue: number,
+): {
+  holdingDetails: HoldingDetails;
+  hasTransaction: boolean;
+} => {
+  if (!shouldRebalance) {
+    return {
+      holdingDetails: currentDetails,
+      hasTransaction: false,
+    }
+  }
+
+  let hasTransaction = false
+  const details = currentDetails.holdings.reduce((details, holding) => {
+    const matched = dailyTickers[holding.tickerId]
+    if (!matched) return details
+
+    const matchedDaily = matched.daily
+    const holdingPercent = (holding.value / details.totalValue) * 100
+
+    const refreshedForMinPercernt = refreshHoldingsForLessThanMinPercent(
+      details, holding, holdingPercent, tickerMinPercent, maxCashValue,
+    )
+    if (refreshedForMinPercernt) {
+      hasTransaction = true
+      return refreshedForMinPercernt
+    }
+
+    const refreshedForMaxPercent = refreshHoldingsForMoreThanMaxPercernt(
+      details, holding, holdingPercent, tickerMaxPercent, maxCashValue, matchedDaily,
+    )
+    if (refreshedForMaxPercent) {
+      hasTransaction = true
+      return refreshedForMaxPercent
+    }
+
+    return details
+  }, currentDetails)
+  return {
+    hasTransaction,
+    holdingDetails: details,
+  }
+}
