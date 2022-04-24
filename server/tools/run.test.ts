11b0b258c5f2936104cf375b9e8d79448664bf40
Baozier <byn9826@gmail.com>
2022-04-24 19:58:01 -0400

    fix test

--- server/tools/run.test.ts
+++ server/tools/run.test.ts
@@ -4,12 +4,12 @@ test('could sleep by seconds', async () => {
   const start = new Date()
   await run.sleep(0.5)
   const end = new Date()
-  expect(end.getTime() - start.getTime()).toBeGreaterThanOrEqual(500)
+  expect(500 - end.getTime() + start.getTime()).toBeLessThanOrEqual(1)
 
   const start1 = new Date()
   await run.sleep(0.2)
   const end1 = new Date()
-  expect(end1.getTime() - start1.getTime()).toBeGreaterThanOrEqual(200)
+  expect(200 - end1.getTime() + start1.getTime()).toBeLessThanOrEqual(1)
 })
 
 test('could loop by async forEach', async () => {
