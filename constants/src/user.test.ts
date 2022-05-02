4ca88764bcc8a9f6fdce4326315abfcfe808da5c
Baozier <byn9826@gmail.com>
2022-05-02 15:21:29 -0400

    Smoothly redirect user between logout and login (#225)

--- constants/src/user.test.ts
+++ constants/src/user.test.ts
@@ -2,6 +2,7 @@ import * as user from './user'
 
 describe('Type', () => {
   test('has defined user types', () => {
+    expect(user.Type.Guest).toBeDefined()
     expect(user.Type.Normal).toBeDefined()
   })
 })
