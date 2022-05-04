b32d0552e07b677bef073cdefad21931d3400a78
Baozier <byn9826@gmail.com>
2022-05-04 16:43:10 -0400

    update test config

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  coverageReporters: [],
+  coverageReporters: ['text'],
   collectCoverageFrom: [
     './client/**/*.{ts,tsx}',
     './server/**/*.{ts,tsx}',
     './constants/**/*.{ts,tsx}',
   ],
+  coveragePathIgnorePatterns: ['/dist/'],
   testPathIgnorePatterns: ['/dist/'],
   setupFiles: ['dotenv/config'],
 }
