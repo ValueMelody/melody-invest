c8a54011911447ea69b025820d361c8f250bcb9e
Baozier <byn9826@gmail.com>
2022-05-21 22:26:09 -0400

    Add footer and contact link (#289)

---
+++ client/src/containers/layouts/elements/HeaderLink.tsx
@@ -0,0 +1,38 @@
+import * as vendorTool from '../../../tools/vendor'
+
+const useStyles = vendorTool.jss.createUseStyles({
+  label: {
+    marginLeft: '0.5rem !important',
+    marginRight: '0.5rem !important',
+  },
+  icon: {
+    marginRight: '0.5rem !important',
+  },
+})
+
+const HeaderLink = ({
+  title,
+  route,
+  icon,
+}: {
+  title?: string,
+  route: string,
+  icon: vendorTool.ui.SemanticICONS,
+}) => {
+  // ------------------------------------------------------------ State --
+  const classes = useStyles()
+
+  return (
+    <vendorTool.router.Link to={route}>
+      <vendorTool.ui.Label className={classes.label}>
+        <vendorTool.ui.Icon
+          name={icon}
+          className={vendorTool.classNames({ [classes.icon]: !!title })}
+        />
+        {title || ''}
+      </vendorTool.ui.Label>
+    </vendorTool.router.Link>
+  )
+}
+
+export default HeaderLink
