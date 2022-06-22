fb679776a2db44a9cea9288e0a8ea7190d99e516
Baozier <byn9826@gmail.com>
2022-06-22 18:41:38 +0800

    Add test for style hooks (#334)

---
+++ client/src/styles/useCommonStyle.test.ts
@@ -0,0 +1,16 @@
+import { renderHook } from '@testing-library/react-hooks'
+import useCommonStyle from './useCommonStyle'
+
+describe('#useCommonStyle', () => {
+  test('could render common style', () => {
+    const { result } = renderHook(() => useCommonStyle())
+    expect(result.current.commonClasses.rowBetween).toBeDefined()
+    expect(result.current.commonClasses.rowAround).toBeDefined()
+    expect(result.current.commonClasses.rowStart).toBeDefined()
+    expect(result.current.commonClasses.rowCenter).toBeDefined()
+    expect(result.current.commonClasses.columnCenter).toBeDefined()
+    expect(result.current.commonClasses.columnStart).toBeDefined()
+    expect(result.current.commonClasses.cursorInfo).toBeDefined()
+    expect(result.current.commonClasses.cursorClickable).toBeDefined()
+  })
+})
