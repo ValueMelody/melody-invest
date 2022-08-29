2d988a6dbc4183fe22661c5facbcd71173756578
Baozier <byn9826@gmail.com>
2022-08-29 19:06:02 +0800

    more test around refetch

--- client/src/containers/general/Privacy.test.tsx
+++ client/src/containers/general/Privacy.test.tsx
@@ -3,18 +3,24 @@ import Privacy from './Privacy'
 import * as requestAdapter from 'adapters/request'
 import * as constants from '@shared/constants'
 
+const fetchMock = jest.fn(async (url: string) => {
+  const baseUrl = 'http://127.0.0.1:3100/system/policy'
+  if (url === `${baseUrl}/${constants.Content.PolicyType.Privacy}`) {
+    return {
+      id: 2,
+      content: 'This is about privacy',
+    }
+  }
+  return null
+})
+
 jest
   .spyOn(requestAdapter, 'sendGetRequest')
-  .mockImplementation(async (url: string) => {
-    const baseUrl = 'http://127.0.0.1:3100/system/policy'
-    if (url === `${baseUrl}/${constants.Content.PolicyType.Privacy}`) {
-      return {
-        id: 2,
-        content: 'This is about privacy',
-      }
-    }
-    return null
-  })
+  .mockImplementation(fetchMock)
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
 
 describe('#Privacy', () => {
   test('could render privacy', async () => {
@@ -23,5 +29,22 @@ describe('#Privacy', () => {
     )
     const content = await waitFor(() => screen.queryByTestId('privacy-content'))
     expect(content?.innerHTML).toBe('This is about privacy')
+    expect(fetchMock).toBeCalledTimes(1)
+  })
+
+  test('do not fetch if content exists', async () => {
+    render(
+      <Privacy />,
+      {
+        store: {
+          resources: {
+            privacyPolicy: 'Privacy already fetched',
+          },
+        },
+      },
+    )
+    const content = await waitFor(() => screen.queryByTestId('privacy-content'))
+    expect(content?.innerHTML).toBe('Privacy already fetched')
+    expect(fetchMock).toBeCalledTimes(0)
   })
 })
