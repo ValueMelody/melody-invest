713e6ba60c2ad4c42e18fea27cf225871423a6a9
Baozier <byn9826@gmail.com>
2023-01-28 23:26:57 -0500

    Move weekly data refresh from cron to cli (#456)

--- server/cli.ts
+++ server/cli.ts
@@ -2,6 +2,7 @@ import * as cacheTask from 'tasks/cache'
 import * as calcTask from 'tasks/calc'
 import * as dateTool from 'tools/date'
 import * as emailTask from 'tasks/email'
+import * as marketAdapter from 'adapters/market'
 import * as runTool from 'tools/run'
 import * as syncTask from 'tasks/sync'
 import * as taskEnum from 'enums/task'
@@ -12,6 +13,24 @@ export const run = async () => {
   const taskName = process.argv[2]
 
   switch (taskName) {
+    case taskEnum.Name.generateWeeklyData: {
+      const cooldown = marketAdapter.getCooldownPerMin()
+
+      const quarter = dateTool.getCurrentQuater()
+      const forceRecheck = false
+      const startTickerId = null
+      await syncTask.syncTickerIncomes(quarter, forceRecheck, startTickerId)
+      await syncTask.syncTickerEarnings(quarter, forceRecheck, startTickerId)
+      await calcTask.calcFinancialMovements()
+      await runTool.sleep(cooldown)
+
+      await syncTask.syncEconomyIndicators()
+      await calcTask.calcIndicatorMovements()
+
+      await calcTask.calcDailyTickers(true)
+      await calcTask.calcTraderPerformances(true)
+      break
+    }
     case taskEnum.Name.generateSystemCaches: {
       await cacheTask.generateSystemCaches()
       break
