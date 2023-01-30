948abfb1625e749b375b80aaca320a9041da123d
Baozier <byn9826@gmail.com>
2023-01-30 16:14:30 -0500

    file loc

--- server/index.ts
+++ server/index.ts
@@ -57,8 +57,8 @@ const getServer = (): HttpServer | HttpsServer => {
 
   if (isHttps) {
     const options = {
-      key: fs.readFileSync(path.join(__dirname, '../key.pem'), 'utf8'),
-      cert: fs.readFileSync(path.join(__dirname, '../cert.pem'), 'utf8'),
+      key: fs.readFileSync(path.join(__dirname, '../../../key.pem'), 'utf8'),
+      cert: fs.readFileSync(path.join(__dirname, '../../../cert.pem'), 'utf8'),
     }
     return server.createServer(options, app)
   }
