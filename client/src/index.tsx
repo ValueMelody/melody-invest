d55071bf91bce73d7834943b26a4b57e60ec1ec7
Baozier <byn9826@gmail.com>
2023-01-30 20:40:24 -0500

    browser router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,7 @@
 import './index.css'
 import 'react-datepicker/dist/react-datepicker.css'
-
+import { BrowserRouter } from 'react-router-dom'
 import Client from './containers/Client'
-import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import ReactDOM from 'react-dom/client'
@@ -10,9 +9,9 @@ import { store } from 'stores'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <Provider store={store}>
       <Client />
     </Provider>
-  </HashRouter>,
+  </BrowserRouter>,
 )
